16fdb240c78712245d2e4fa51cf8c640cc7f5fdf
Actiol <Jonas.s_HansDita17@gmx.de>
2022-09-25 00:13:03 +0200

    Add project files.

---
+++ Blender Version Selector/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Blender_Version_Selector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ComboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonall = new System.Windows.Forms.Button();
+            this.buttoncurrent = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ComboBox1
+            // 
+            this.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBox1.FormattingEnabled = true;
+            this.ComboBox1.Location = new System.Drawing.Point(13, 12);
+            this.ComboBox1.Name = "ComboBox1";
+            this.ComboBox1.Size = new System.Drawing.Size(121, 21);
+            this.ComboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "a";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(41, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonall
+            // 
+            this.buttonall.Location = new System.Drawing.Point(76, 91);
+            this.buttonall.Name = "buttonall";
+            this.buttonall.Size = new System.Drawing.Size(58, 23);
+            this.buttonall.TabIndex = 4;
+            this.buttonall.Text = "All";
+            this.buttonall.UseVisualStyleBackColor = true;
+            this.buttonall.Click += new System.EventHandler(this.buttonall_Click);
+            // 
+            // buttoncurrent
+            // 
+            this.buttoncurrent.Location = new System.Drawing.Point(12, 91);
+            this.buttoncurrent.Name = "buttoncurrent";
+            this.buttoncurrent.Size = new System.Drawing.Size(58, 23);
+            this.buttoncurrent.TabIndex = 5;
+            this.buttoncurrent.Text = "Current";
+            this.buttoncurrent.UseVisualStyleBackColor = true;
+            this.buttoncurrent.Click += new System.EventHandler(this.buttoncurrent_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(146, 123);
+            this.Controls.Add(this.buttoncurrent);
+            this.Controls.Add(this.buttonall);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ComboBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Version Selector";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ComboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonall;
+        private System.Windows.Forms.Button buttoncurrent;
+    }
+}
+
---
+++ Blender Version Selector/Form1.cs
@@ -0,0 +1,180 @@
+﻿using IWshRuntimeLibrary;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace Blender_Version_Selector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string path = @"C:\Program Files\Blender Foundation";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ComboBox1.Items.Clear();
+            string userName = Environment.UserName;
+            string[] filesindirectory = Directory.GetDirectories(path);
+            int amount = 0;
+
+            CreateShortcut("Blender Version Selector", @"C:\Users\" + userName + @"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\", Path.Combine(Directory.GetCurrentDirectory(), System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName), "0");
+
+            foreach (string subdir in filesindirectory)
+            {
+
+                amount++;
+                ComboBox1.Items.Add(subdir.Remove(0, path.Length + 1));
+
+            }
+            if (amount == 1) label1.Text = "1 Version found";
+            else
+                label1.Text = amount.ToString() + " Versions found";
+            ComboBox1.SelectedIndex = amount - 1;
+
+
+            this.Height = 136;
+            buttonall.Hide();
+            buttoncurrent.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] filesindirectory = Directory.GetDirectories(path);
+
+            if (ComboBox1.Text != "")
+            {
+                foreach (string subdir in filesindirectory)
+                {
+
+                    if (subdir.Contains(ComboBox1.Text))
+                    {
+                        string ExecPath = subdir + @"\blender.exe";
+                        Debug.WriteLine(ExecPath);
+                        Process.Start(ExecPath);
+                        break;
+
+                    }
+                    Close();
+                }
+
+            }
+            else Debug.WriteLine("No Path was selected");
+
+        }
+
+        bool extented = false;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!extented)
+            {
+                extented = true;
+                this.Height = 162;
+                buttonall.Show();
+                buttoncurrent.Show();
+            }
+            else
+            {
+                extented = false;
+                this.Height = 136;
+                buttonall.Hide();
+                buttoncurrent.Hide();
+            }
+        }
+
+        public static void CreateShortcut(string shortcutName, string shortcutPath, string targetFileLocation, string desc)
+        {
+            string shortcutLocation = System.IO.Path.Combine(shortcutPath, shortcutName + ".lnk");
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutLocation);
+            if (desc != "0")
+            {
+                shortcut.Description = "A shortcut to " + desc + " generated using Actiol's Blender Version Selector";
+            }
+            else
+            {
+                shortcut.Description = "A shortcut to Actiol's Blender Version Selector";
+            }
+            // The description of the shortcut
+            // shortcut.IconLocation = @"c:\myicon.ico";           // The icon of the shortcut
+            shortcut.TargetPath = targetFileLocation;                 // The path of the file that will launch when the shortcut is run
+            shortcut.Save();                                    // Save the shortcut
+        }
+
+        private void buttoncurrent_Click(object sender, EventArgs e)
+        {
+            if ((MessageBox.Show("Do you want to overwrite the default Blender shortcut to version " + ComboBox1.Text.Replace("Blender ", "") + "?", "Set Version " + ComboBox1.Text.Replace("Blender ", "") + " as default?", MessageBoxButtons.YesNo, MessageBoxIcon.Question)) == DialogResult.Yes)
+            {
+
+                string userName = Environment.UserName;
+                string[] filesindirectory = Directory.GetDirectories(path);
+
+                if (ComboBox1.Text != "")
+                {
+                    ClearDirectory();
+
+                    foreach (string subdir in filesindirectory)
+                    {
+
+                        if (subdir.Contains(ComboBox1.Text))
+                        {
+                            string ExecPath = subdir + @"\blender.exe";
+
+                            CreateShortcut("Blender", @"C:\Users\" + userName + @"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Blender\", ExecPath, ComboBox1.Text);
+
+                            break;
+
+                        }
+                    }
+                }
+                else Debug.WriteLine("No Path was selected");
+            }
+        }
+        public static void ClearDirectory()
+        {
+            string userName = Environment.UserName;
+            System.IO.DirectoryInfo di = new DirectoryInfo(@"C:\Users\" + userName + @"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Blender\");
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+        }
+
+
+        private void buttonall_Click(object sender, EventArgs e)
+        {
+            if ((MessageBox.Show("Do you want to create a shortcut for each Blender version?", "Create a shortcut for each version?", MessageBoxButtons.YesNo, MessageBoxIcon.Question)) == DialogResult.Yes)
+            {
+                string userName = Environment.UserName;
+                string[] filesindirectory = Directory.GetDirectories(path);
+
+                if (ComboBox1.Text != "")
+                {
+                    ClearDirectory();
+
+                    foreach (string subdir in filesindirectory)
+                    {
+                        string ExecPath = subdir + @"\blender.exe";
+
+                        CreateShortcut(subdir.Remove(0, path.Length + 1), @"C:\Users\" + userName + @"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Blender\", ExecPath, subdir.Remove(0, path.Length + 1));
+                    }
+                }
+                else Debug.WriteLine("No Path was selected");
+            }
+        }
+    }
+}
